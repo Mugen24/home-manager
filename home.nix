00e1293c08f5855ff6007c7970e6a64ea9442510
John Nguyen <John.nguyen1022@gmail.com>
2024-09-20 13:34:35 +1000

    Revert polybar changes from main

--- home.nix
+++ home.nix
@@ -11,14 +11,14 @@ in
   
 
 
-  systemd.user.services.polybar = {
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
-  services.polybar = {
-    enable = true;
-    script = "polybar &";
-    config = /home/mugen/.config/home-manager/config/polybar/config.ini;
-  };
+  # systemd.user.services.polybar = {
+  #   Install.WantedBy = [ "graphical-session.target" ];
+  # };
+  # services.polybar = {
+  #   enable = true;
+  #   script = "polybar &";
+  #   config = /home/mugen/.config/home-manager/config/polybar/config.ini;
+  # };
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
