75365c698d7fae78a0cf524ee18717dc30301c7a
John Nguyen <John.nguyen1022@gmail.com>
2024-12-09 16:28:56 +1100

    Finished nvim terminal module

--- Programing/Editor/terminal_modules.lua
+++ Programing/Editor/terminal_modules.lua
@@ -1,17 +1,149 @@
 local window_h = vim.fn.winheight("0") / 3
 
-vim.cmd(string.format("rightb %ssplit term://zsh", window_h))
-local buff_name = vim.fn.bufname(2)
-vim.cmd.close()
+local terminals = {}
+local curr_term = nil
+
+-- TODO: rework this into an event listener
+local toggle = false
+
+function CreateTerm()
+    vim.cmd(string.format("rightb %ssplit term://zsh", window_h))
+end
+
+function CloseTerm()
+    if vim.fn.winnr("$") > 1 then
+        vim.cmd.close()
+    end
+end
+
+function AddTerm()
+    CreateTerm()
+    curr_term = vim.fn.bufname("$")
+    table.insert(terminals, curr_term)
+end
+
+function OpenTerm()
+    if table.getn(terminals) == 0 then
+        AddTerm()
+    else
+        vim.cmd(string.format("rightb %ssplit %s", window_h, curr_term))
+    end
+end
 
 
-vim.keymap.set("t", "\\", vim.cmd.close)
-vim.keymap.set("n", "\\",
+vim.keymap.set("t", "<c-t>", function ()
+    CloseTerm()
+    AddTerm()
+    vim.cmd.startinsert()
+end)
+
+vim.keymap.set("t", "<c-w>", function()
+    -- NOTE passing 0 doesn't work 
+    local deletedBuf = vim.fn.bufname("%")
+    local bufHandle = vim.fn.bufnr(deletedBuf)
+
+    local deletedBufIndex = nil
+    -- Delete buff from terminals
+    for i, bufName in ipairs(terminals) do
+        if bufName == deletedBuf then
+            deletedBufIndex = i
+            break
+        end
+    end
+
+    if deletedBufIndex == nil then
+        vim.print("Cannot find deletedBuf in term")
+        vim.print(deletedBuf)
+        vim.print(terminals)
+        return
+    else
+        table.remove(terminals, deletedBufIndex)
+    end
+    --
+    -- Get buff from buf list
+    if bufHandle == -1 then
+        print("Cannot find deletedBuf in ls buf")
+        vim.print(bufHandle)
+        vim.print(terminals)
+        return
+    else
+        vim.api.nvim_win_close(0, true)
+        if deletedBufIndex > 1 then
+            curr_term = terminals[deletedBufIndex - 1]
+            OpenTerm()
+        else
+            toggle = false
+        end
+    end
+
+end)
+
+
+vim.keymap.set({"n", "t"}, "\\",
     function ()
-        vim.cmd(string.format("rightb %ssplit %s", window_h, buff_name))
-        vim.cmd.startinsert()
+        if toggle == false then
+            toggle = true
+            OpenTerm()
+            vim.cmd.startinsert()
+            vim.print("h", toggle)
+            vim.print("false ->", toggle)
+        elseif toggle == true then
+            toggle = false
+            CloseTerm()
+            vim.print("h", toggle)
+            vim.print("true ->", toggle)
+        end
     end,
     {
         desc = "Invoke terminal"
     }
 )
+
+----------------------------
+local finders = require "telescope.finders"
+local pickers = require "telescope.pickers"
+local previewers = require "telescope.previewers"
+local sorters = require "telescope.sorters"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local bufferPicker = function(opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "Terminals",
+        finder = finders.new_table({
+            results = terminals
+        }),
+        sorters = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+
+            local selection = action_state.get_selected_entry()
+            vim.print(selection)
+            if selection == nil then
+                return
+            end
+
+            if selection[1] == nil then
+                vim.print("nil selection")
+                return
+            else
+                vim.print("running")
+                CloseTerm()
+                curr_term = selection[1] -- Get buff name
+                OpenTerm()
+            end
+
+          end)
+          return true
+        end,
+    }):find()
+end
+
+vim.keymap.set("t", "<leader>o", function ()
+    bufferPicker()
+end)
+
+
---
+++ Programing/Editor/test.lua
@@ -0,0 +1,5 @@
+local a = {
+    1,
+    x = 10}
+vim.print(a)
+vim.print(a[1])
